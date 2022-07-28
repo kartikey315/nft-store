fdc9bd78f4f9e40ba13b70b12be5ca89ca4460d2
kartikey315 <kartikeykiroula6789@gmail.com>
2022-07-28 18:37:39 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    
+    <div style={{minHeight: '100vh' }}className='bg-neutral-900'>
+      <nav className='border-b p-6'>
+        <div>
+        <p className='text-4xl font-bold text-white'>NFTStore</p>
+        </div>
+        <div className='flex justify-between mt-4'>
+          <Link href="/">
+            <a className="text-xl mr-6 text-lime-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className='text-xl mr-6 text-lime-500'>
+              Sell NFTs
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className='text-xl mr-6 text-lime-500'>
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/dashboard">
+            <a className='text-xl mr-6 text-lime-500'>
+              Creator Dashboard
+            </a>
+          </Link>
+        </div>
+
+      </nav>
+      <Component {...pageProps} />
+  </div>
+  
+  )
 }
 
 export default MyApp
